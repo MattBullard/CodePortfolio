514520fcff96f459b80aaa8ad561f6c7092e3730
Matt Bullard <suffolkmagic@yahoo.com>
2015-01-14 12:28:33 -0600

    Create Autoloader.php
    This file is my PHP class auto loader.  Its function is to automatically load PHP class files at run time when needed,  This avoids having to have several INCLUDE commands at the start of every PHP file in your application.

---
+++ PHP/webApp/webapp/Autoloader.php
@@ -0,0 +1,50 @@
+<?php
+
+WebApp_Autoloader::Register();
+
+
+/**
+ * Autoloader
+ *
+ * @category	
+ * @package		
+ * @copyright	Copyright (c) 2014
+ */
+class WebApp_Autoloader
+{
+	/**
+	 * Register the Autoloader with SPL
+	 *
+	 */
+	public static function Register() {
+		if (function_exists('__autoload')) {
+			//	Register any existing autoloader function with SPL, so we don't get any clashes
+			spl_autoload_register('__autoload');
+		}
+		//	Register ourselves with SPL
+		return spl_autoload_register(array('WebApp_Autoloader', 'Load'));
+	}	//	function Register()
+
+
+	/**
+	 * Autoload a class identified by name
+	 *
+	 * @param	string	$pClassName		Name of the object to load
+	 */
+	public static function Load($pClassName){
+		if ((class_exists($pClassName)) || (strpos($pClassName, 'WebApp') !== 0)) {
+			return FALSE; // Either already loaded, or not a valid class request
+		}
+
+		$pObjectFilePath = WEBAPP . str_replace('_',DIRECTORY_SEPARATOR,$pClassName) . '.php';
+
+		if ((file_exists($pObjectFilePath) === false) || (is_readable($pObjectFilePath) === false)) {
+			return FALSE; // Can't load
+		}
+
+		require($pObjectFilePath);
+	}	//	function Load()
+
+}
+
+?>

c2eda53953d2accda3b49252a43f49101110ace9
Matt Bullard <suffolkmagic@yahoo.com>
2015-01-29 14:09:07 -0600

    Update script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,3 +1,33 @@
-function test() {
-
-}
+$(window).load(function(){
+	
+	var cnt = 0;
+	
+	$(window).scroll(function() {
+	  if($(this).scrollTop() != 0) {
+		$('#to-top').fadeIn(); 
+	  } else {
+		$('#to-top').fadeOut();
+	  }
+	});
+	$('#to-top').click(function() {
+		$('body,html').animate({scrollTop:0},"fast");
+	});
+	$('#matl_add').click(function() {  
+		return !$('#select1 option:selected').remove().appendTo('#select2');  
+	});  
+	$('#select1').dblclick(function() {  
+		return !$('#select1 option:selected').remove().appendTo('#select2');  
+	});  
+	$('#matl_remove').click(function() {  
+		return !$('#select2 option:selected').remove().appendTo('#select1');  
+	});
+	$('#select2').dblclick(function() {  
+		return !$('#select2 option:selected').remove().appendTo('#select1');  
+	});
+	$('#researchform').submit(function() {  
+		$('#select2 option').each(function(i) {  
+			$(this).attr("selected", "selected");  
+		});  
+	});
+	
+});

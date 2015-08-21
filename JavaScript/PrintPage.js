9c793bcd06a4aa2221a605afad94ee9f8b82548a
Matt Bullard <suffolkmagic@yahoo.com>
2015-08-21 07:45:18 -0500

    Create PrintPage.js

---
+++ JavaScript/PrintPage.js
@@ -0,0 +1,21 @@
+<script type="text/javascript">
+        function PrintWindow()
+        {                     
+           window.print();            
+           CheckWindowState(); 
+        }
+        
+        function CheckWindowState()
+        {            
+            if(document.readyState=="complete")
+            {
+                window.close();  
+            }
+            else
+            {            
+                setTimeout("CheckWindowState()", 2000)
+            }
+        }    
+        
+       PrintWindow();
+</script>
